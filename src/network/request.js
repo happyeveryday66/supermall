ed3d09dcde83f2d520459e6904cf6fcbb689876a
guoqiang.zhao <guoqiang.zhao@ucarinc.com>
2021-08-30 16:48:49 +0800

    Home离开时记录状态和位置

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config){
 
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: '加coderwhy老师微信',
+    baseURL: '',
     timeout: 5000
   })
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -65,13 +65,24 @@
         currentType: 'pop',
         isShowBackTop: false,
         tabOffsetTop: 0,
-        isTabFixed: false
+        isTabFixed: false,
+        saveY: 0
       }
     },
     computed: {
       showGoods() {
         return this.goods[this.currentType].list
       }
+    },
+    destroyed() {
+      console.log('Home destroyed');
+    },
+    activated() {
+      this.$refs.scroll.scrollTo(0, this.saveY, 0)
+      this.$refs.scroll.refresh()
+    },
+    deactivated() {
+      this.saveY = this.$refs.scroll.getScrollY()
     },
     created() {
       // 1.请求多个数据
